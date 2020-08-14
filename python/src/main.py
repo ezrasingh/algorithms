fb45995ebd6088f9e6420d8fab7212b1107d4130
Ezra Singh <ezra@liontech.nyc>
2020-08-13 20:37:58 -0400

    Added driver code

---
+++ python/src/main.py
@@ -0,0 +1,8 @@
+from . import structures
+from . import algorithms
+
+def main():
+    print("Running script...")
+
+if __name__ == '__main__':
+    main()
---
+++ typescript/src/main.ts
@@ -0,0 +1,8 @@
+import * from './structures';
+import * from './algorithms';
+
+async function main(): Promise<any>{
+    console.log("Running script...");
+};
+
+main();
